2e0178cc5773e9c41bfa6b3b3600d70930304b82
JoJobani <154671628+JoJobani@users.noreply.github.com>
2024-05-23 09:41:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 const MINE = '*'
 const FLAG = 'F'
 const EMPTY = ' '
+const NORMAL = '😃'
+const LOSE = '🤯'
+const WIN = '😎'
 
 var firstClick = true
 var gBoard
@@ -12,6 +15,7 @@ var gLevel = {
 }
 var gGame = {
     isOn: false,
+    lives: 3,
     shownCount: 0,
     markedCount: 0,
     secsPassed: 0
@@ -24,7 +28,10 @@ function onInit() {
     gGame.shownCount = 0
     gGame.markedCount = 0
     gGame.secsPassed = 0
+    gGame.lives = 3
     gGame.isOn = true
+    var elRestart = document.querySelector('.restart')
+    elRestart.innerText = NORMAL
 }
 
 function chooseLevel(elBtn) {
@@ -67,6 +74,8 @@ function renderBoard(board) {
     var boardContainer = document.querySelector('.board')
     boardContainer.innerHTML = ''
     boardContainer.style.gridTemplateColumns = `repeat(${gLevel.SIZE}, 50px)`
+    var elLives = document.querySelector('.lives')
+    elLives.innerHTML = `lives: ${gGame.lives}`
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
             var elCell = document.createElement('div')
@@ -128,22 +137,30 @@ function countNegs(cellI, cellJ, board) {
 function onCellClicked(i, j) {
     if (!gGame.isOn) return
     if (gBoard[i][j].isMarked) return
+    //first click handling
     if (firstClick) {
         firstClick = false
         placeMines(gBoard, i, j)
         setMinesNegsCount(gBoard)
     }
+    //normal handling
     gBoard[i][j].isShown = true
-    gGame.shownCount++
+    if (!gBoard[i][j].isMine) gGame.shownCount++
+    //mine handling
     if (gBoard[i][j].isMine) {
-        for (var a = 0; a < gBoard.length; a++) {
-            for (var b = 0; b < gBoard[0].length; b++) {
-                if (gBoard[a][b].isMine) {
-                    gBoard[a][b].isShown = true
+        gGame.lives--
+        if (!gGame.lives) {
+            var elRestart = document.querySelector('.restart')
+            elRestart.innerText = LOSE
+            for (var a = 0; a < gBoard.length; a++) {
+                for (var b = 0; b < gBoard[0].length; b++) {
+                    if (gBoard[a][b].isMine) {
+                        gBoard[a][b].isShown = true
+                    }
                 }
             }
+            gGame.isOn = false
         }
-        gGame.isOn = false
     }
     expandShown(gBoard, i, j)
     checkGameOver()
@@ -163,21 +180,25 @@ function onCellMarked(i, j) {
 
 function checkGameOver() {
     var isWon = false
+    console.log(gGame.shownCount)
+    console.log((gLevel.SIZE ** 2) - gLevel.MINES)
     for (var i = 0; i < gBoard.length; i++) {
         for (var j = 0; j < gBoard[0].length; j++) {
-            if (gGame.shownCount === (gLevel.SIZE ** 2 - gLevel.MINES)) {
+            if (gGame.shownCount === ((gLevel.SIZE ** 2) - gLevel.MINES)) {
                 isWon = true
             }
         }
     }
     if (isWon) {
         gGame.isOn = false
-        alert("victory!")
+        var elRestart = document.querySelector('.restart')
+        elRestart.innerText = WIN
     }
 }
 
 function expandShown(board, i, j) {
     if (board[i][j].minesAroundCount !== 0) return
+    if (board[i][j].isMine) return
     for (var a = i - 1; a <= i + 1; a++) {
         if (a < 0 || a >= board.length) continue
         for (var b = j - 1; b <= j + 1; b++) {
@@ -187,7 +208,7 @@ function expandShown(board, i, j) {
                 if (!board[a][b].isShown) gGame.shownCount++
                 board[a][b].isShown = true
                 if (board[a][b].minesAroundCount === 0) {
-                    expandShown(board,a,b)
+                    expandShown(board, a, b)
                 }
             }
         }
